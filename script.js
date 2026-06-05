4d3fb435a4aff477b2533e8651c69b8e5fa0597a
Goran Mitev <gokimk@gmail.com>
2026-06-05 15:56:04 -0400

    Increase slider max to 1 billion MKD (GROSS_MAX=1,000,000,000)
    - GROSS_MAX=1B, NET_MAX=800M
    - Mid point scaled to 100M / 80M for the non-linear slider curve
      (60% of slider now covers up to 100M for usable control)
    - Bumped cache buster to v=11

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ var exchangeRates = { MKD: 1, EUR: 61.5, USD: 57.0 };
 var currencySymbols = { MKD: 'ден', EUR: '€', USD: '$' };
 
 var mode = 'gross'; // 'gross' or 'net'
-var GROSS_MIN = 40000, GROSS_MAX = 100000000;
-var NET_MIN = 26000, NET_MAX = 80000000;
+var GROSS_MIN = 40000, GROSS_MAX = 1000000000;
+var NET_MIN = 26000, NET_MAX = 800000000;
 
 function formatMoney(value) {
     var rate = exchangeRates[currency];
@@ -48,7 +48,7 @@ function formatMoney(value) {
 
 function sliderToSalary(pos) {
     var min = mode === 'gross' ? GROSS_MIN : NET_MIN;
-    var mid = mode === 'gross' ? 10000000 : 8000000;
+    var mid = mode === 'gross' ? 100000000 : 80000000;
     var max = mode === 'gross' ? GROSS_MAX : NET_MAX;
     if (pos <= 600) {
         var t = pos / 600;
